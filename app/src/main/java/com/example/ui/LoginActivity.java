d42bcd6c7ded4ce85c51af5c2ee12d1d33b11cef
ducnt1210 <127038551+ducnt1210@users.noreply.github.com>
2023-09-27 22:02:03 +0700

    complete login, register, splashScreen, icon logic and temp UI,

--- app/src/main/java/com/example/ui/LoginActivity.java
+++ app/src/main/java/com/example/ui/LoginActivity.java
@@ -2,6 +2,10 @@ package com.example.ui;
 
 import static android.service.controls.ControlsProviderService.TAG;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
 import android.content.Intent;
 import android.graphics.Color;
 import android.net.Uri;
@@ -10,10 +14,282 @@ import android.util.Log;
 import android.view.View;
 import android.widget.Toast;
 
-import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
-import androidx.appcompat.app.AppCompatActivity;
+import com.example.ui.Model.UserModel;
+import com.example.ui.databinding.ActivityLoginBinding;
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.common.api.ApiException;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthCredential;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.GoogleAuthProvider;
+import com.google.firebase.auth.UserInfo;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
 
-public class LoginActivity {
+import java.util.Objects;
 
+import cn.pedant.SweetAlert.SweetAlertDialog;
+
+public class LoginActivity extends AppCompatActivity {
+
+    SweetAlertDialog sweetAlertDialog;
+    ActivityLoginBinding binding;
+    FirebaseAuth firebaseAuth;
+    GoogleSignInOptions gso;
+    GoogleSignInClient gsc;
+    FirebaseStorage storage;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setTheme(R.style.Theme_UI);
+        binding = ActivityLoginBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        firebaseAuth = FirebaseAuth.getInstance();
+        firebaseAuth.signOut();
+//        TODO: comment 2 above to keep user sign in
+
+        sweetAlertDialog = new SweetAlertDialog(this, SweetAlertDialog.PROGRESS_TYPE);
+        sweetAlertDialog.getProgressHelper().setBarColor(Color.parseColor("#A5DC86"));
+        sweetAlertDialog.setCancelable(false);
+
+        storage = FirebaseStorage.getInstance();
+
+        gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestIdToken(getString(com.firebase.ui.auth.R.string.default_web_client_id))
+                .requestEmail()
+                .requestProfile()
+                .build();
+        gsc = GoogleSignIn.getClient(this, gso);
+        binding.ggButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                ggSignin();
+            }
+        });
+
+        firebaseAuth = FirebaseAuth.getInstance();
+        FirebaseUser user = firebaseAuth.getCurrentUser();
+        if (user != null) {
+            Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+            intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+            startActivity(intent);
+        }
+
+        binding.login.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View view) {
+                String email = binding.email.getText().toString();
+                String password = binding.password.getText().toString().trim();
+                sweetAlertDialog = new SweetAlertDialog(LoginActivity.this, SweetAlertDialog.PROGRESS_TYPE);
+                sweetAlertDialog.getProgressHelper().setBarColor(Color.parseColor("#A5DC86"));
+                sweetAlertDialog.setCancelable(false);
+                sweetAlertDialog.show();
+                if (!password.equals("") && !email.equals("")) {
+                    firebaseAuth.signInWithEmailAndPassword(email, password)
+                            .addOnSuccessListener(new OnSuccessListener<AuthResult>() {
+                                @Override
+                                public void onSuccess(AuthResult authResult) {
+                                    checkUserSignIn();
+                                }
+                            })
+                            .addOnFailureListener(new OnFailureListener() {
+                                @Override
+                                public void onFailure(@NonNull Exception e) {
+                                    sweetAlertDialog.dismissWithAnimation();
+                                    Toast.makeText(LoginActivity.this, e.getMessage(), Toast.LENGTH_SHORT).show();
+                                }
+                            });
+                } else {
+                    sweetAlertDialog.dismissWithAnimation();
+                    Toast.makeText(LoginActivity.this, "Fill in email and password", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
+        binding.resetPassword.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String email = binding.email.getText().toString();
+
+
+                sweetAlertDialog = new SweetAlertDialog(LoginActivity.this, SweetAlertDialog.PROGRESS_TYPE);
+                sweetAlertDialog.getProgressHelper().setBarColor(Color.parseColor("#A5DC86"));
+                sweetAlertDialog.setCancelable(false);
+                sweetAlertDialog.setTitle("Sending email");
+                sweetAlertDialog.show();
+                if (!email.equals("")) {
+                    firebaseAuth.sendPasswordResetEmail(email)
+                            .addOnSuccessListener(new OnSuccessListener<Void>() {
+                                @Override
+                                public void onSuccess(Void unused) {
+                                    sweetAlertDialog.dismissWithAnimation();
+                                    Toast.makeText(LoginActivity.this, "Password reset send to email", Toast.LENGTH_SHORT).show();
+                                }
+                            })
+                            .addOnFailureListener(new OnFailureListener() {
+                                @Override
+                                public void onFailure(@NonNull Exception e) {
+                                    sweetAlertDialog.dismissWithAnimation();
+                                    Toast.makeText(LoginActivity.this, e.getMessage(), Toast.LENGTH_SHORT).show();
+                                }
+                            });
+                } else {
+                    sweetAlertDialog.dismissWithAnimation();
+                    Toast.makeText(LoginActivity.this, "Fill in email to send verification", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
+        binding.toSignupActivity.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(LoginActivity.this, SignupActivity.class));
+                finishAffinity();
+            }
+        });
+    }
+
+    private void ggSignin() {
+        Intent intent = gsc.getSignInIntent();
+        startActivityForResult(intent, 1000);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == 1000) {
+            Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(data);
+            try {
+                GoogleSignInAccount account = task.getResult(ApiException.class);
+                Log.d(TAG, "firebaseAuthWithGoogle" + account.getId());
+                firebaseAuthWithGoogle(account.getIdToken());
+            } catch (ApiException e) {
+                Toast.makeText(this, e.getMessage(), Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
+
+    private void firebaseAuthWithGoogle(String idToken) {
+        AuthCredential credential = GoogleAuthProvider.getCredential(idToken, null);
+        firebaseAuth.signInWithCredential(credential)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            Log.d(TAG, "signInWithCredential:success");
+                            FirebaseUser user = firebaseAuth.getCurrentUser();
+                            boolean newUser = Objects.requireNonNull(task.getResult().getAdditionalUserInfo()).isNewUser();
+                            if (newUser) {
+                                assert user != null;
+                                String name = null, phoneNumber = null, email = null;
+                                Uri profileImage = Uri.parse("android.resource://com.example.financialapp/" + R.drawable.default_profile_picture);
+                                for (UserInfo profile : user.getProviderData()) {
+                                    name = profile.getDisplayName();
+                                    phoneNumber = profile.getPhoneNumber();
+                                    email = profile.getEmail();
+                                    if (profile.getPhotoUrl() != null)
+                                        profileImage = profile.getPhotoUrl();
+                                }
+                                final Uri defaultImage = profileImage;
+                                UserModel ggUser = new UserModel(user.getUid(), name, phoneNumber, email);
+//                                ggUser.setSignIn(true);
+                                //                            TODO: return or fix setSignIn when have signOut
+                                FirebaseFirestore.getInstance().collection("User").document(ggUser.getId()).set(ggUser)
+                                        .addOnSuccessListener(new OnSuccessListener<Void>() {
+                                            @Override
+                                            public void onSuccess(Void unused) {
+//                                                Uri defaultImage = Uri.parse("android.resource://com.example.ui/" + R.drawable.default_profile_picture);
+                                                StorageReference reference = storage.getReference().child("images/" + ggUser.getId());
+                                                reference.putFile(defaultImage).addOnCompleteListener(new OnCompleteListener<UploadTask.TaskSnapshot>() {
+                                                    @Override
+                                                    public void onComplete(@NonNull Task<UploadTask.TaskSnapshot> task) {
+                                                        Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+                                                        startActivity(intent);
+                                                        finish();
+                                                    }
+                                                });
+                                            }
+                                        }).addOnFailureListener(new OnFailureListener() {
+                                            @Override
+                                            public void onFailure(@NonNull Exception e) {
+                                                Log.w(TAG, "signInWithCredential:failure", task.getException());
+                                                Toast.makeText(LoginActivity.this, "Login failed", Toast.LENGTH_SHORT).show();
+                                            }
+                                        });
+                            } else {
+                                FirebaseFirestore.getInstance().collection("User").document(user.getUid()).get().addOnSuccessListener(new OnSuccessListener<DocumentSnapshot>() {
+                                    @Override
+                                    public void onSuccess(DocumentSnapshot documentSnapshot) {
+                                        UserModel tempGGUser = documentSnapshot.toObject(UserModel.class);
+                                        if (tempGGUser.isSignIn()) {
+                                            Toast.makeText(LoginActivity.this, "This account already been signed in!", Toast.LENGTH_SHORT).show();
+                                            FirebaseAuth.getInstance().signOut();
+                                            gsc.signOut();
+                                            LoginActivity.this.recreate();
+                                        } else {
+//                                            tempGGUser.setSignIn(true);
+                                            //                            TODO: return or fix setSignIn when have signOut
+                                            FirebaseFirestore.getInstance().collection("User").document(tempGGUser.getId()).set(tempGGUser).addOnCompleteListener(new OnCompleteListener<Void>() {
+                                                @Override
+                                                public void onComplete(@NonNull Task<Void> task) {
+                                                    Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+                                                    startActivity(intent);
+                                                    finish();
+                                                }
+                                            });
+                                        }
+                                    }
+                                });
+                            }
+                        } else {
+                            Log.w(TAG, "signInWithCredential:failure", task.getException());
+                            Toast.makeText(LoginActivity.this, "Login failed", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+    }
+
+    private void checkUserSignIn() {
+        FirebaseFirestore.getInstance().collection("User").document(FirebaseAuth.getInstance().getCurrentUser().getUid())
+                .get()
+                .addOnSuccessListener(new OnSuccessListener<DocumentSnapshot>() {
+                    @Override
+                    public void onSuccess(DocumentSnapshot documentSnapshot) {
+                        UserModel user = documentSnapshot.toObject(UserModel.class);
+                        assert user != null;
+                        if (user.isSignIn()) {
+                            Toast.makeText(LoginActivity.this, "This account already been signed in!", Toast.LENGTH_SHORT).show();
+                            FirebaseAuth.getInstance().signOut();
+                            sweetAlertDialog.dismissWithAnimation();
+                        } else {
+//                            user.setSignIn(true);
+//                            TODO: return or fix setSignIn when have signOut
+                            FirebaseFirestore.getInstance().collection("User").document(user.getId()).set(user)
+                                    .addOnSuccessListener(new OnSuccessListener<Void>() {
+                                        @Override
+                                        public void onSuccess(Void unused) {
+                                            sweetAlertDialog.dismissWithAnimation();
+                                            Toast.makeText(LoginActivity.this, "Login Successfully!", Toast.LENGTH_SHORT).show();
+                                            finish();
+                                            startActivity(new Intent(LoginActivity.this, MainActivity.class));
+                                        }
+                                    });
+                        }
+                    }
+                });
+    }
 }
---
+++ app/src/main/java/com/example/ui/SignupActivity.java
@@ -0,0 +1,114 @@
+package com.example.ui;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.graphics.Color;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+import com.example.ui.Model.UserModel;
+import com.example.ui.databinding.ActivitySignupBinding;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+
+import java.util.Objects;
+
+import cn.pedant.SweetAlert.SweetAlertDialog;
+
+public class SignupActivity extends AppCompatActivity {
+
+    String allCountryRegex = "^(\\+\\d{1,3}( )?)?((\\(\\d{1,3}\\))|\\d{1,3})[- .]?\\d{3,4}[- .]?\\d{4}$";
+    SweetAlertDialog sweetAlertDialog;
+    ActivitySignupBinding binding;
+    FirebaseAuth firebaseAuth;
+    FirebaseFirestore firebaseFirestore;
+    FirebaseStorage storage;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = ActivitySignupBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        firebaseAuth = FirebaseAuth.getInstance();
+        firebaseFirestore = FirebaseFirestore.getInstance();
+        storage = FirebaseStorage.getInstance();
+
+        sweetAlertDialog = new SweetAlertDialog(this, SweetAlertDialog.PROGRESS_TYPE);
+        sweetAlertDialog.getProgressHelper().setBarColor(Color.parseColor("#A5DC86"));
+        sweetAlertDialog.setCancelable(false);
+
+        binding.signup.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View view) {
+                String fullName = binding.fullName.getText().toString();
+                String number = binding.mobileNumber.getText().toString();
+                String email = binding.email.getText().toString().trim();
+                String password = binding.password.getText().toString();
+                String confirmPassword = binding.confirmPassword.getText().toString();
+
+                sweetAlertDialog = new SweetAlertDialog(SignupActivity.this, SweetAlertDialog.PROGRESS_TYPE);
+                sweetAlertDialog.getProgressHelper().setBarColor(Color.parseColor("#A5DC86"));
+                sweetAlertDialog.setCancelable(false);
+                sweetAlertDialog.show();
+                if (!password.equals("") && !email.equals("") && !confirmPassword.equals("") && !fullName.equals("") && !number.equals("")) {
+                    if (number.matches(allCountryRegex)) {
+                        if (password.equals(confirmPassword)) {
+                            firebaseAuth.createUserWithEmailAndPassword(email, password)
+                                    .addOnSuccessListener(new OnSuccessListener<AuthResult>() {
+
+                                        @Override
+                                        public void onSuccess(AuthResult authResult) {
+                                            Toast.makeText(SignupActivity.this, "Sign up successfully", Toast.LENGTH_SHORT).show();
+                                            startActivity(new Intent(SignupActivity.this, LoginActivity.class));
+                                            sweetAlertDialog.dismissWithAnimation();
+
+                                            firebaseFirestore.collection("User")
+                                                    .document(Objects.requireNonNull(FirebaseAuth.getInstance().getUid()))
+                                                    .set(new UserModel(Objects.requireNonNull(FirebaseAuth.getInstance().getUid()), fullName, number, email));
+                                            Uri defaultImage = Uri.parse("android.resource://com.example.ui/" + R.drawable.default_profile_picture);
+                                            StorageReference reference = storage.getReference().child("images/" + Objects.requireNonNull(FirebaseAuth.getInstance().getUid()));
+                                            reference.putFile(defaultImage);
+                                        }
+                                    })
+                                    .addOnFailureListener(new OnFailureListener() {
+
+                                        @Override
+                                        public void onFailure(@NonNull Exception e) {
+                                            Toast.makeText(SignupActivity.this, e.getMessage(), Toast.LENGTH_SHORT).show();
+                                            sweetAlertDialog.dismissWithAnimation();
+                                        }
+                                    });
+                        } else {
+                            sweetAlertDialog.dismissWithAnimation();
+                            Toast.makeText(SignupActivity.this, "Reconfirm your password", Toast.LENGTH_SHORT).show();
+                        }
+                    } else {
+                        sweetAlertDialog.dismissWithAnimation();
+                        Toast.makeText(SignupActivity.this, "The phone number is badly formatted", Toast.LENGTH_SHORT).show();
+                    }
+                } else {
+                    sweetAlertDialog.dismissWithAnimation();
+                    Toast.makeText(SignupActivity.this, "Please fill in all the information", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
+        binding.toLoginActivity.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(SignupActivity.this, LoginActivity.class));
+                finishAffinity();
+            }
+        });
+    }
+}
